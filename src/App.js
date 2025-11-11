291a43dae8c7f966c87cd053bdb938004088dcbc
Mathilde Schmid <mathilde.schmid@orange.fr>
2025-11-11 18:02:08 +0100

    Register Page front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import RegisterPage from "./pages/auth/register/RegisterPage";
+import LoginPage from "./pages/auth/login/LoginPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/auth/register" element={<RegisterPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+      </Routes>
     </div>
   );
 }
